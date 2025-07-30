f4f7aaa5bcfb9710592758973d29099bcaa4f213
Mukaram Awan <141765944+mukaramawan@users.noreply.github.com>
2025-07-30 17:26:39 +0500

    Project Initialization

---
+++ src/main/java/com/example/Employee_CRUD_Project_with_SpringBoot_React/EmployeeCrudProjectWithSpringBootReactApplication.java
@@ -0,0 +1,13 @@
+package com.example.Employee_CRUD_Project_with_SpringBoot_React;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeCrudProjectWithSpringBootReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeCrudProjectWithSpringBootReactApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Employee_CRUD_Project_with_SpringBoot_React/EmployeeCrudProjectWithSpringBootReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Employee_CRUD_Project_with_SpringBoot_React;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeCrudProjectWithSpringBootReactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
